8210cd5264f6677ff56adae4e272c4141d57b7c3
Krettelle NAVARRO <k.navarro@wanadoo.fr>
2020-04-09 10:39:41 +0200

    connect to bdd

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const express = require('express');
-const app = express();
 
+const app = require('./app');
 const PORT = process.env.PORT || 3000;
 
-const userRouter = require('./routes/user');
+const mongoose = require ('mongoose');
+const DB_USER = 'api-rest-social';
+const DB_PWD = 'Titi98';
+const DB_HOST = 'cluster0-dixr6.mongodb.net';
+const DB_NAME = 'social_network';
 
-app.use('', userRouter);
+const dbUrl = `mongodb+srv://${DB_USER}:${DB_PWD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`;
 
+//mongodb+srv://<username>:<password>@cluster0-dixr6.mongodb.net/test?retryWrites=true&w=majority
+
+//Connection à la BDD
+mongoose.connect(dbUrl, {useNewUrlParser:true, useUnifiedTopology:true}, () =>{
+  console.log(`conneexion à la bdd ${DB_NAME} ok.`)
+} )
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}.`));
