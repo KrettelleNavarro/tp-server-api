3bbe9b9fe4003aae7bbf6010ef7ab4165cc18fbf
Krettelle NAVARRO <k.navarro@wanadoo.fr>
2020-04-09 10:06:21 +0200

    user controller

--- index.js
+++ index.js
@@ -3,19 +3,9 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-const userRouter = express.Router();
+const userRouter = require('./routes/user');
 
 app.use('', userRouter);
 
-userRouter.get('/api/users', (req, res) => {
-  const users = require('./fixtures');
-  res.status(200).json({ users });
-});
-
-userRouter.get('/api/users/:id', (req, res) => {
-  const user = require('./fixtures').users[req.params.id];
-
-  res.status(200).json({ user });
-});
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}.`));
---
+++ routes/user.js
@@ -0,0 +1,10 @@
+const express = require ('express');
+const router = express.Router();
+
+const userCtrl = require('../controllers/user');
+ 
+router.get('/api/users',userCtrl.getUsers );
+ 
+router.get('/api/users/:id', userCtrl.getUser );
+
+module.exports = router;
